496f09c58f1f798ecf42d47bcb8f8f8cf371ac95
Honeybadgrr <quinnstahlman@gmail.com>
2025-09-08 15:21:09 -0700

    SwerveSubsystem and FieldNav initial setup

---
+++ src/main/java/frc/robot/FieldNavigation.java
@@ -0,0 +1,155 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.Degrees;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+
+/** Add your docs here. */
+public class FieldNavigation {
+static double botCenterToScorerOffset = Inches.of(2.0).in(Meters);
+    static double botCenterToRearX = Inches.of((30/2.0)+4+0.25+0.5+1.0).in(Meters);
+    static double pidApproachOffset = Inches.of(20.25+12).in(Meters);
+    static double coralY = Inches.of(15/2.0).in(Meters);
+    //These are right relative from the tag's pose facing out  from the reef
+    static Transform2d coralLeft = new Transform2d(new Pose2d(), new Pose2d(botCenterToRearX, Inches.of(7.0).in(Meters), new Rotation2d(Degrees.of(0))));
+    static Transform2d coralRight = new Transform2d(new Pose2d(), new Pose2d(botCenterToRearX, Inches.of(-4.0).in(Meters), new Rotation2d(Degrees.of(0))));
+    static Transform2d coralMid = new Transform2d(new Pose2d(), new Pose2d(botCenterToRearX, 0.0, new Rotation2d(Degrees.of(0))));
+    static Transform2d reefAlgae = new Transform2d(new Pose2d(), new Pose2d(botCenterToRearX, 0, new Rotation2d(Degrees.of(0))));
+    static Transform2d coralSource = new Transform2d(new Pose2d(), new Pose2d(botCenterToRearX, 0, new Rotation2d(Degrees.of(180))));
+    static Transform2d coralApproachOffsetLeft = new Transform2d(new Pose2d(), new Pose2d(pidApproachOffset, Inches.of(7.0).in(Meters), new Rotation2d(Degrees.of(0))));
+    static Transform2d coralApproachOffsetRight = new Transform2d(new Pose2d(), new Pose2d(pidApproachOffset, Inches.of(-4).in(Meters), new Rotation2d(Degrees.of(0))));
+
+
+
+    public static List<Pose2d> tagsReef = new ArrayList<>(){{
+        add(AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape).getTagPose(6).get().toPose2d());
+        add(AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape).getTagPose(7).get().toPose2d());
+        add(AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape).getTagPose(8).get().toPose2d());
+        add(AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape).getTagPose(9).get().toPose2d());
+        add(AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape).getTagPose(10).get().toPose2d());
+        add(AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape).getTagPose(11).get().toPose2d());
+        
+        add(AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape).getTagPose(17).get().toPose2d());
+        add(AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape).getTagPose(18).get().toPose2d());
+        add(AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape).getTagPose(19).get().toPose2d());
+        add(AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape).getTagPose(20).get().toPose2d());
+        add(AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape).getTagPose(21).get().toPose2d());
+        add(AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape).getTagPose(22).get().toPose2d());
+    }};
+
+    public static List<Pose2d> tagsSource = new ArrayList<>(){{
+        add(AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape).getTagPose(1).get().toPose2d());
+        add(AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape).getTagPose(2).get().toPose2d());
+        add(AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape).getTagPose(13).get().toPose2d());
+        add(AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape).getTagPose(12).get().toPose2d());
+    }};
+
+    public static List<Pose2d> tagsProcessor = new ArrayList<>(){{
+        add(AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape).getTagPose(3).get().toPose2d());
+        add(AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape).getTagPose(16).get().toPose2d());
+    }};
+
+    public Field2d field = new Field2d();
+
+    public static Pose2d blueBarge = AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape).getTagPose(14).get().toPose2d();
+    public static Pose2d redBarge = AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape).getTagPose(14).get().toPose2d();
+
+
+
+    public Pose2d getNearestReef(Pose2d currentPose){
+        return currentPose.nearest(tagsReef);
+    }
+
+    public static Pose2d getNearestSource(Pose2d currentPose){
+        return currentPose.nearest(tagsSource);
+    }
+
+    public static Pose2d getNearestProcessor(Pose2d currentPose){
+        var alliance = DriverStation.getAlliance().orElse(Alliance.Blue);
+        //TODO: Filter/select this one by alliance to prevent possible driver confusion in edge cases
+        return currentPose.nearest(tagsProcessor);
+    }
+
+    //FROM PERSPECTIVE OF APRIL TAG, if we look from outside it is right
+    public static Pose2d getCoralLeft(Pose2d currentPose){
+        var nearest = currentPose.nearest(tagsReef);
+        return nearest.transformBy(coralLeft);
+    }
+
+    public static Pose2d getCoralMid(Pose2d currentPose){
+        var nearest = currentPose.nearest(tagsReef);
+        return nearest.transformBy(coralMid);
+    }
+
+    public static Pose2d getOffsetCoralLeft(Pose2d currentPose){
+        var nearest = currentPose.nearest(tagsReef);
+        return nearest.transformBy(coralApproachOffsetLeft);
+    }
+
+    //SEE ABOVE
+    public static Pose2d getCoralRight(Pose2d currentPose){
+        var nearest = currentPose.nearest(tagsReef);
+        return nearest.transformBy(coralRight);
+    }
+    public static Pose2d getOffsetCoralRight(Pose2d currentPose){
+        var nearest = currentPose.nearest(tagsReef);
+        return nearest.transformBy(coralApproachOffsetRight);
+    }
+
+    public static Pose2d getCoralSource(Pose2d currentPose){
+        var nearest = currentPose.nearest(tagsSource);
+        return nearest.transformBy(coralSource);
+
+        
+    }
+
+    public static Pose2d getReefAlgae(Pose2d currentPose){
+        var nearest = currentPose.nearest(tagsReef);
+        return nearest.transformBy(reefAlgae);
+    }
+
+    public enum Branch {
+        A,
+        B,
+        C;
+    }
+
+    private static Pose2d getBranchRed(Branch target){
+        switch (target) {
+            case A: return AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape).getTagPose(6).get().toPose2d();//FIXME
+        }
+        return new Pose2d();
+    }
+
+    private static Pose2d getBranchBlue(Branch target){
+        return new Pose2d();
+    }
+
+    public static Pose2d getBranch(Branch target){
+        if(DriverStation.getAlliance().orElse(Alliance.Blue)==Alliance.Blue){
+            return getBranchBlue(target);
+        }
+        else{
+            return getBranchRed(target);
+        }
+    }
+
+
+
+}
--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -4,14 +4,276 @@
 
 package frc.robot.subsystems;
 
+import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.Meters;
+
+import java.io.File;
+import java.util.Set;
+import java.util.function.DoubleSupplier;
+
+import com.studica.frc.AHRS;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.DeferredCommand;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.FieldNavigation;
+import swervelib.SwerveDrive;
+import swervelib.parser.SwerveParser;
+
 
 public class SwerveSubsystem extends SubsystemBase {
   /** Creates a new SwerveSubsystem. */
-  public SwerveSubsystem() {}
+  public Field2d odometryField = new Field2d();
+
+  
+  double maximumSpeed = 5.033;
+
+  SwerveDrive swerveDrive;
+
+  //2.24-1.88
+  SimpleMotorFeedforward feedforward = new SimpleMotorFeedforward(0.1, 2.75, 0.35);
+
+  public SwerveSubsystem() {
+    SmartDashboard.putData("odometryField", odometryField);
+    File swerveJsonDirectory = new File(Filesystem.getDeployDirectory(),"swervedrive");
+        try
+        {
+          swerveDrive = new SwerveParser(swerveJsonDirectory).createSwerveDrive(maximumSpeed);
+          // Alternative method if you don't want to supply the conversion factor via JSON files.
+          // swerveDrive = new SwerveParser(directory).createSwerveDrive(maximumSpeed, angleConversionFactor, driveConversionFactor);
+        } catch (Exception e)
+        {
+          throw new RuntimeException(e);
+
+  }}
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    swerveDrive.updateOdometry();
+    odometryField.setRobotPose(swerveDrive.getPose());
+
+    SmartDashboard.putNumber("serve/wheelencoder", swerveDrive.getModules()[0].getPosition().distanceMeters);
+    
+    SmartDashboard.putNumber("swerve/heading", swerveDrive.getOdometryHeading().getDegrees());
+    SmartDashboard.putNumber("swerve/x", swerveDrive.getPose().getX());
+    SmartDashboard.putNumber("swerve/y", swerveDrive.getPose().getY());
+    
+    //SmartDashboard.putBoolean("Navx/Connected", isGyroConnected());
+    //SmartDashboard.putBoolean("Navx/Callibrating", isGyroCallibrating());
+
+    }  
+
+
+  private Command driveCommand(DoubleSupplier translationX, DoubleSupplier translationY, DoubleSupplier angularRotationX){
+    return run(() -> {
+      // Make the robot move
+      swerveDrive.drive(new Translation2d(translationX.getAsDouble() * swerveDrive.getMaximumChassisVelocity(),
+                                          translationY.getAsDouble() * swerveDrive.getMaximumChassisVelocity()),
+                        angularRotationX.getAsDouble() * swerveDrive.getMaximumChassisAngularVelocity(),
+                        true,
+                        false);
+    });
   }
+
+  public Command driveCommandRobotRelative(DoubleSupplier translationX, DoubleSupplier translationY, DoubleSupplier angularRotationX){
+    return run(() -> {
+      // Make the robot move
+      swerveDrive.drive(new Translation2d(translationX.getAsDouble() * swerveDrive.getMaximumChassisVelocity(),
+                                          translationY.getAsDouble() * swerveDrive.getMaximumChassisVelocity()),
+                        angularRotationX.getAsDouble() * swerveDrive.getMaximumChassisAngularVelocity(),
+                        false,
+                        false);
+    });
+  }
+
+private void stop(){
+    swerveDrive.setChassisSpeeds(new ChassisSpeeds());
+  }
+
+public boolean isGyroConnected(){
+    var navx = (AHRS)swerveDrive.getGyro().getIMU();
+    if(navx.isConnected()){
+      return true;
+    }
+    return false;
+  }
+  public boolean isGyroCallibrating(){
+    var navx = (AHRS)swerveDrive.getGyro().getIMU();
+    if(navx.isCalibrating()){
+      return true;
+    }
+    return false;
+  }
+
+  public Pose2d getPose(){
+    return swerveDrive.getPose();
+  }
+  public Rotation2d getHeading(){
+    return swerveDrive.getGyro().getRotation3d().toRotation2d();
+  }
+
+  public Command pathToCoralLeft(){
+    return new DeferredCommand(()->privatePathToPose(FieldNavigation.getCoralLeft(getPose())), Set.of(this));
+  }
+
+  public Command pathToCoralRight(){
+    return new DeferredCommand(()->privatePathToPose(FieldNavigation.getCoralRight(getPose())), Set.of(this));
+  }
+
+  public Command pathToCoralSource(){
+    return new DeferredCommand(()->privatePathToPose(FieldNavigation.getCoralSource(getPose())), Set.of(this));
+  }
+
+  public Command pathToOffsetLeft(){
+    return new DeferredCommand(()->privatePathToOffset(FieldNavigation.getOffsetCoralLeft(getPose())), Set.of(this));
+  }
+
+  public Command pathToOffsetRight(){
+    return new DeferredCommand(()->privatePathToOffset(FieldNavigation.getOffsetCoralRight(getPose())), Set.of(this));
+  }
+
+  public Command pidToCoralLeft(){
+    return new DeferredCommand(()->pidToPoseFastCommand(FieldNavigation.getCoralLeft(getPose())), Set.of(this));
+  }
+
+  public Command pidToCoralRight(){
+    return new DeferredCommand(()->pidToPoseFastCommand(FieldNavigation.getCoralRight(getPose())), Set.of(this));
+  }
+
+  public Command pidToCoralLeftHuman(){
+    return new DeferredCommand(()->privatePathToPosePrecise(FieldNavigation.getCoralLeft(getPose())), Set.of(this));
+  }
+
+  public Command pidToCoralRightHuman(){
+    return new DeferredCommand(()->privatePathToPosePrecise(FieldNavigation.getCoralRight(getPose())), Set.of(this));
+  }
+
+  public Command pidToCoralSource(){
+    return new DeferredCommand(()->pidToPoseFastCommand(FieldNavigation.getCoralSource(getPose())), Set.of(this));
+  }
+
+  
+
+  public boolean isNearEnoughToPID(Pose2d target){
+    var distance = target.getTranslation().getDistance(getPose().getTranslation());
+    var result = distance <= Inches.of(35).in(Meters);
+    SmartDashboard.putBoolean("swerve/isNearEnough",result);
+    return result;
+  }
+
+  public boolean isNearEnoughToPIDAuto(Pose2d target){
+    var distance = target.getTranslation().getDistance(getPose().getTranslation());
+    var result = distance <= Inches.of(5.0).in(Meters);
+    SmartDashboard.putBoolean("swerve/isNearEnough",result);
+    return result;
+  }
+
+  public boolean isNearEnoughToPIDPrecise(Pose2d target){
+    var distance = target.getTranslation().getDistance(getPose().getTranslation());
+    var result = distance <= Inches.of(48).in(Meters);
+    SmartDashboard.putBoolean("swerve/isNearEnough",result);
+    return result;
+  }
+
+  public boolean isNearEnoughToScore(Pose2d target){
+    var distance = target.getTranslation().getDistance(getPose().getTranslation());
+    return distance <= Inches.of(1).in(Meters);
+  }
+
+  public boolean isNear(Pose2d target,double distance){
+    var delta = target.getTranslation().getDistance(getPose().getTranslation());
+    return delta <= Inches.of(distance).in(Meters);
+  }
+
+
+
+  private void pidToPoseFast(Pose2d pose){
+    final double transltionP = 3.0*1.2;
+    final double thetaP = 2.0*4*1.2 ;
+
+    double clamp = 2.0;
+
+    Pose2d delta = pose.relativeTo(swerveDrive.getPose());
+
+    swerveDrive.setChassisSpeeds(new ChassisSpeeds(
+      MathUtil.clamp(delta.getX()*transltionP,-clamp, clamp),
+      MathUtil.clamp(delta.getY()*transltionP,-clamp,clamp),
+      delta.getRotation().getRadians()*thetaP
+    ));
+
+    odometryField.getObject("pidtarget").setPose(pose);
+    SmartDashboard.putNumber("swerve/pid/deltax", pose.getX());
+    SmartDashboard.putNumber("swerve/pid/deltay", pose.getY());
+
+  }
+
+  private void pidToPosePrecise(Pose2d pose){
+    final double transltionP = 3.0*1.2*1.5*1.5*1.5;
+    final double thetaP = 2.0*4*1.2;
+    
+
+    double clamp = 0.75;
+
+    Pose2d delta = pose.relativeTo(swerveDrive.getPose());
+
+    swerveDrive.setChassisSpeeds(new ChassisSpeeds(
+      MathUtil.clamp(delta.getX()*transltionP,-clamp, clamp),
+      MathUtil.clamp(delta.getY()*transltionP,-clamp,clamp),
+      delta.getRotation().getRadians()*thetaP
+    ));
+
+    SmartDashboard.putNumber("swerve/pidTargetPoseX", pose.getX());
+    SmartDashboard.putNumber("swerve/pidTargetPoseY", pose.getY());
+
+  }
+
+  public Command pidToPoseFastCommand(Pose2d poseSupplier){
+    return run(()->pidToPoseFast(poseSupplier)).finallyDo(()->stop());
+  }
+
+  public Command pidToPosePreciseCommand(Pose2d poseSupplier){
+    return run(()->pidToPosePrecise(poseSupplier));
+  }
+
+  private Command privatePathToOffset(Pose2d pose){
+    // return pidToPoseCommand(pose).until(()->isNear(pose, 12.0)).withTimeout(4.5);
+    return pidToPoseFastCommand(pose)
+      .until(()->isNear(pose, 12.0))
+      .withTimeout(4.5)
+    ;
+  }
+
+  private Command privatePathToPosePrecise(Pose2d pose){
+    return Commands.sequence(
+      pidToPoseFastCommand(pose).until(()->isNearEnoughToPIDPrecise(pose)).withTimeout(4.5),
+      pidToPosePreciseCommand(pose).until(()->isNearEnoughToScore(pose)).withTimeout(1.5),
+      new InstantCommand(this::stop,this)
+    );
+  }
+  private Command privatePathToPose(Pose2d pose){
+    return Commands.sequence(
+      pidToPoseFastCommand(pose)
+      .until(()->isNearEnoughToPID(pose))
+      .withTimeout(5),
+      pidToPosePreciseCommand(pose).until(()->isNearEnoughToScore(pose)).withTimeout(3.0),
+      new InstantCommand(this::stop,this)
+    );
+  }
+
+  
+
+
+
 }
